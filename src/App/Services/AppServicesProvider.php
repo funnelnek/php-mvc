767918e035635eb396b9aefd72335edd6466efc3
= <=>
2021-12-02 20:34:15 +0000

    added AppServicesProvider

---
+++ src/App/Services/AppServicesProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+use Funnelnek\App\Services\CacheControlService;
+use Funnelnek\App\Services\CatalogService;
+use Funnelnek\App\Services\ProductService;
+
+return [
+    CacheControlService::class,
+    CatalogService::class,
+    ProductService::class
+];
